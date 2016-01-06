f0671b2173365743e807b915b86f08bbcc1dbdc3
Martbov <Mart@RN-145-97-167-38.eduroam.rug.nl>
2016-01-06 15:20:45 +0100

    Added first classifier

--- project/finalproject.py
+++ project/finalproject.py
@@ -1,11 +1,17 @@
 #!usr/bin/python3.4
 
-import sys, os
+import sys
 import glob
 from collections import defaultdict
 import xml.etree.ElementTree as ET
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.cross_validation import cross_val_score
 
 def fileRead(language):
+	"""Reads in data from the directories"""
 	informationDict = defaultdict(list)
 	documents = []
 	genders = []
@@ -16,24 +22,56 @@ def fileRead(language):
 		truthvalues = [value for value in line.strip().split(':::')]
 		informationDict[truthvalues[0]] = (truthvalues[1], truthvalues[2])
 	fileList = [xmlfile for xmlfile in glob.glob('training/' + language + '/*.xml')]
+	ageDict = {'XX-XX':'0', '18-24':'1', '25-34':'2', '35-49':'3', '50-XX':'4'}
 	for xmlfile in fileList:
 		tree = ET.parse(xmlfile)
 		root = tree.getroot()
-		print(root.attrib['id'])
 		for child in root:
 			documents.append(child.text)
 			genders.append(informationDict[root.attrib['id']][0])
-			ages.append(informationDict[root.attrib['id']][1])
+			ages.append(ageDict[informationDict[root.attrib['id']][1]])
 			authors.append(root.attrib['id'])
-		
 	return documents, genders, ages, authors
+
+def identity(x):
+	"""Dummy function"""
+	return x
+
+def naiveBayesClassfier(doc, gen, age, aut):
+	splitPoint = int(0.80*len(doc))
+	xTrain = doc[:splitPoint]
+	yTrain = gen[:splitPoint]
+	xTest = doc[splitPoint:]
+	yTest = gen[splitPoint:]
+
+	vec = TfidfVectorizer(preprocessor = identity, tokenizer = identity)
+	classifier = Pipeline([('vec', vec), ('cls', MultinomialNB())])
+	classifier.fit(xTrain, yTrain)
+	yGuess = classifier.predict(xTest)
+
+	print(classification_report(yTest, yGuess))
+
+def crossvalidationClassifier(x, y):
+	vec = TfidfVectorizer(preprocessor = identity, tokenizer = identity)
+	classifier = Pipeline([('vec', vec), ('cls', MultinomialNB())])
+	
+	crossvalidatorPrecision = cross_val_score(classifier, x, y=y, cv=10, n_jobs=-1, scoring="precision_weighted")
+	crossvalidatorRecall = cross_val_score(classifier, x, y=y, cv=10, n_jobs=-1, scoring="recall_weighted")
+	crossvalidatorFscore = cross_val_score(classifier, x, y=y, cv=10, n_jobs=-1, scoring="f1_weighted")
+
+	print("Precision:", sum(crossvalidatorPrecision)/len(crossvalidatorPrecision))
+	print("Recall:", sum(crossvalidatorRecall)/len(crossvalidatorRecall))
+	print("F1-score:", sum(crossvalidatorFscore)/len(crossvalidatorFscore))
 
 if __name__ == '__main__':
 	languages = ['dutch', 'english', 'italian', 'spanish']
-	if len(sys.argv) == 2 and sys.argv[1] in languages:
-		language = sys.argv[1]
-		fileRead(language)
+	dutDOC, dutGEN, dutAGE, dutAUT = fileRead(languages[0])
+	engDOC, engGEN, engAGE, engAUT = fileRead(languages[1])
+	itaDOC, itaGEN, itaAGE, itaAUT = fileRead(languages[2])
+	spaDOC, spaGEN, spaAGE, spaAUT = fileRead(languages[3])
 
-	else:
-		print("Usage: python finalproject.py <language>", file=sys.stderr)
-		print("Accepted languages are 'dutch', 'english', 'italian' and 'spanish'", file=sys.stderr)
+	#naiveBayesClassfier(engDOC, engGEN, engAGE, engAUT)
+	
+	crossvalidationClassifier(spaDOC, spaAGE)
+
+
