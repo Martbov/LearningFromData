bb5e02539a43d6866bcf0f0883dbff387dbdf009
Martbov <mart_bov@hotmail.com>
2016-01-12 15:28:38 +0100

    First attempts with feature selection

--- project/finalproject.py
+++ project/finalproject.py
@@ -9,6 +9,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.cross_validation import cross_val_score
+import nltk
 
 def fileRead(language):
 	"""Reads in data from the directories"""
@@ -22,29 +23,45 @@ def fileRead(language):
 		truthvalues = [value for value in line.strip().split(':::')]
 		informationDict[truthvalues[0]] = (truthvalues[1], truthvalues[2])
 	fileList = [xmlfile for xmlfile in glob.glob('training/' + language + '/*.xml')]
-	ageDict = {'XX-XX':'0', '18-24':'1', '25-34':'2', '35-49':'3', '50-XX':'4'}
 	for xmlfile in fileList:
 		tree = ET.parse(xmlfile)
 		root = tree.getroot()
 		for child in root:
-			documents.append(child.text)
+			tweet = child.text.strip()
+			documents.append(child.text.strip())
 			genders.append(informationDict[root.attrib['id']][0])
-			ages.append(ageDict[informationDict[root.attrib['id']][1]])
+			ages.append(informationDict[root.attrib['id']][1])
 			authors.append(root.attrib['id'])
 	return documents, genders, ages, authors
 
-def identity(x):
-	"""Dummy function"""
-	return x
+def tokenizer(document):
+	"""Tokenizer function"""
+	return ' '.join(nltk.word_tokenize(document))
+
+def preprocessing(document):
+	"""Small preprocessing function"""
+	oldWords = document.split()
+	newWords = []
+	punctList = ['!', '?', '@' ',', '.', '(', ')', '#', '/', '\\', '|']
+	for word in oldWords:
+		if word.startswith('#'):
+			newWords.append('#hashtag')
+		elif word.startswith('http'):
+			newWords.append('LINK')
+		else:
+			newWords.append(word)
+	return ' '.join(newWords)
+
 
 def naiveBayesClassfier(doc, gen, age, aut):
-	splitPoint = int(0.80*len(doc))
+	"""A NaiveBayes classifier function"""
+	splitPoint = int(0.8*len(doc))
 	xTrain = doc[:splitPoint]
-	yTrain = gen[:splitPoint]
 	xTest = doc[splitPoint:]
-	yTest = gen[splitPoint:]
+	yTrain = age[:splitPoint]
+	yTest = age[splitPoint:]
 
-	vec = TfidfVectorizer(preprocessor = identity, tokenizer = identity)
+	vec = TfidfVectorizer(preprocessor = preprocessing, tokenizer = None, sublinear_tf = True)
 	classifier = Pipeline([('vec', vec), ('cls', MultinomialNB())])
 	classifier.fit(xTrain, yTrain)
 	yGuess = classifier.predict(xTest)
@@ -70,8 +87,38 @@ if __name__ == '__main__':
 	itaDOC, itaGEN, itaAGE, itaAUT = fileRead(languages[2])
 	spaDOC, spaGEN, spaAGE, spaAUT = fileRead(languages[3])
 
-	#naiveBayesClassfier(engDOC, engGEN, engAGE, engAUT)
+
+	#crossvalidationClassifier(spaDOC, spaAGE)
+	naiveBayesClassfier(engDOC, engGEN, engAGE, engAUT)
 	
-	crossvalidationClassifier(spaDOC, spaAGE)
+
+
+
+
+
+
+
+
+
+
+"""
+	allDOC = []
+	allGEN = []
+	allAGE = []
+	allAUT = []
+	allDOC.extend(dutDOC)
+	allDOC.extend(engDOC)
+	allDOC.extend(itaDOC)
+	allDOC.extend(spaDOC)
+	allGEN.extend(dutGEN)
+	allGEN.extend(engGEN)
+	allGEN.extend(itaGEN)
+	allGEN.extend(spaGEN)
+	allAGE.extend(dutAGE)
+	allAGE.extend(engAGE)
+	allAGE.extend(itaAGE)
+	allAGE.extend(spaAGE)"""
+
+	
 
 
